a3a52c3bd882cba8813b635bee2748443eded6b6
Re4lect <museflect@gmail.com>
2023-06-11 21:42:45 +0300

    task1 done

--- Task2/src/Main.java
+++ Task2/src/Main.java
@@ -3,10 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         checkText(getText());
-
-
     }
-
     private static String getText(){
         System.out.println("Введите текст на английском, не менее 100 слов: ");
         return new Scanner(System.in).nextLine();
@@ -19,9 +16,7 @@ public class Main {
             if (!String.valueOf(text2.charAt(i)).matches("[A-z ]")){
                 System.out.println("Введен недопустимый символ!");
                 System.exit(1);
-
         }}
-
         if (text2.split(" ").length<100){
             System.out.println("Вы ввели менее 100 слов!");
             System.exit(1);
--- Task3/src/Main.java
+++ Task3/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 public class Main {
+
     public static void main(String[] args) {
-        //todo ФИО
+        fullNames();
+    }
+    private static void fullNames(){
+        final String REGEX_NAME ="([А-я]+-*[А-я]* [А-я]+-*[А-я]* [А-я]+-*[А-я]*)";
+        System.out.println("Введите ФИО: ");
+        String name = new Scanner(System.in).nextLine();
+        if(!name.matches(REGEX_NAME)
+        ){
+            System.out.println("Введенный текст не является ФИО!");
+            System.exit(1);
+        }
+        System.out.println("Фамилия: "+name.split(" ")[0]+"\n"+
+                "Имя: "+name.split(" ")[1]+"\n"+
+                "Отчество: "+name.split(" ")[2]+"\n");
+
     }
 }
