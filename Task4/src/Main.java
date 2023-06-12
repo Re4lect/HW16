9ec6f7df296c38f1a360c1962c807d7ce2aca1f2
Re4lect <museflect@gmail.com>
2023-06-12 23:02:40 +0300

    done

--- Task4/src/Main.java
+++ Task4/src/Main.java
@@ -9,8 +9,13 @@ public class Main {
         System.out.println("Введите телевонный номер: ");
         String number = new Scanner(System.in).nextLine();
         String newNumber = number.replaceAll("[., \\-()+]","");
-        if((newNumber.length()==11)&&((newNumber.charAt(0)=='7')||(newNumber.charAt(0)=='8'))){
-            System.out.println(newNumber.replaceAll(TEST,"+7 ($2) $3 $4-$5"));
+        String lastNumber = "";
+        if(newNumber.length()==10){
+            lastNumber = "8"+newNumber;
+        } else {lastNumber = newNumber;
+        }
+        if((lastNumber.length()==11)&&((lastNumber.charAt(0)=='7')||(lastNumber.charAt(0)=='8'))){
+            System.out.println(lastNumber.replaceAll(TEST,"+7 ($2) $3 $4-$5"));
         }else {System.out.println("Ошибка");}
 
     }
